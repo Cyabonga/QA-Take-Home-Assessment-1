c8a500f64769af0a6d052e33c97e75f812bc943b
Cyabonga <siyabongasuccessnkosi@hotmail.com>
2023-07-18 08:16:34 +0200

    Update factorial_validation.py

--- factorial_validation.py
+++ factorial_validation.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 # Start the Selenium WebDriver
 driver = webdriver.Chrome("/Users/siyabongankosi/PycharmProjects/QA-Take-Home-Assessment/drivers/chromedriver")
 
@@ -9,7 +10,7 @@ driver.get("http://localhost:6464/factorial")
 
 # Fill in the number input
 number_input = driver.find_element(By.ID, "number")
-number_input.send_keys("9")
+number_input.send_keys("7")
 
 # Submit the form
 submit_button = driver.find_element(By.ID, "getFactorial")
@@ -18,10 +19,13 @@ submit_button.click()
 # Get the result
 result_div = driver.find_element(By.ID, "resultDiv")
 result = result_div.text
+result_style = result_div.get_attribute("style")
 
 # Check if the result is correct
-expected_result = "The factorial of 9 is: 362880"
-assert result == expected_result,  f"expected_result{expected_result}, but got {result}"
+expected_result = "The factorial of 7 is: 5040"
+expected_style = 'color: rgb(0, 0, 0);'
+assert result == expected_result, f"expected_result{expected_result}, but got {result}"
+assert result_style == expected_style, f"Expected style: {expected_style}, but got {result_style}"
 
 # Close the browser
 driver.quit()
